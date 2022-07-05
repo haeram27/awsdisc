8d45e2b1252d26fd18c48301701feecb7b29ea01
haeram27 <haeram27@gmail.com>
2022-07-05 09:50:02 +0900

    add making nested json test

---
+++ client/test/make_nested_json_test.go
@@ -0,0 +1,30 @@
+package test
+
+import (
+	"awsdisc/client/util"
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+func TestMakeNestedJson(t *testing.T) {
+	inner1 := `{"intValue": 1234}`
+	inner2 := `{"boolValue": true}`
+
+	// consolidate
+	data := make(map[string]interface{})
+	var innerJson interface{}
+	json.Unmarshal([]byte(inner1), &innerJson)
+	data["inner1"] = innerJson
+	json.Unmarshal([]byte(inner2), &innerJson)
+	data["inner2"] = innerJson
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println("could not marshal json: ", err)
+		return
+	}
+
+	fmt.Println(string(jsonData))
+	util.PrintPrettyJson(jsonData)
+}
